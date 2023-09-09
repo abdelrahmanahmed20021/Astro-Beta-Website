7af192d1903499f146f9ef109be98346f18c0578
AbdalrahmanEldeep <abdo.eslam2021@gmail.com>
2023-09-10 00:25:37 +0300

    UPPEDN React and Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react({
+      include: ["**/react/*"],
+    }),
+    vue(),
+  ],
+});
